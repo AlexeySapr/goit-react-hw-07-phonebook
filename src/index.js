5e76d966a4057f1c5f0ca485f82def8ecdf09733
Alexey Sapr <alexeysapr@gmail.com>
2022-01-29 18:12:17 +0200

    Add init files

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { store, persistor } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { ThemeProvider } from '@emotion/react';
+import { theme } from 'constants/theme';
+import 'index.css';
+import App from 'App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
